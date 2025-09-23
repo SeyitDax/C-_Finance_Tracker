5932731925428b29eb5dace9f38044aec4b33a2b
Seyit Ahmet <ahmetsaddemir@gmail.com>
2025-09-23 14:11:08 +0300

    Added functionality to the Console Control Panel

--- AccountControl.cs
+++ AccountControl.cs
@@ -10,10 +10,39 @@ namespace AccountControl{
     }
 
     public class ConsoleControl : IAccountControl{
-        public void ControlPanel()
+        public void ControlPanel(Account acc)
         {
             Console.WriteLine("
             WELCOME! \n What would you like to do today? \n 1.CheckBalance \n 2.CheckDebt \n 3.Deposit \n 4.Withdraw \n 5.Pay Debt");
+
+            string? choice = Console.ReadLine();
+
+            switch(choice){
+                case("1"):
+                    CheckBalance(acc);
+                    break;
+                case("2"):
+                    CheckDebt(acc);
+                    break;
+                case("3"):
+                    Console.WriteLine("How much money would you like to deposit?");
+                    string? result = Console.ReadLine();
+                    decimal.TryParse(result, out decimal amount) ? Console.WriteLine("Please enter a valid amount!");
+                    Deposit(acc, amount);
+                    break;
+                case("4"):
+                    Console.WriteLine("How much money would you like to withdraw?");
+                    string? result = Console.ReadLine();
+                    decimal.TryParse(result, out decimal amount) ? Console.WriteLine("Please enter a valid amount!");
+                    Withdraw(acc, amount);
+                    break;
+                case("5"):
+                    Console.WriteLine("How much money would you like to pay?");
+                    string? result = Console.ReadLine();
+                    decimal.TryParse(result, out decimal amount) ? Console.WriteLine("Please enter a valid amount!");
+                    PayDebt(acc, amount);
+                    break;
+            }
         }
 
         public CheckBalance(Account acc)
